77271249f6acae75a2c7f6d4246803722f87e219
Kyziur <kyziurs@gmail.com>
2019-10-10 20:00:09 +0200

    Added docker configuration for frontend .

--- src/ProjectTemplate.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProjectTemplate.Core
-{
-    public class Class1
-    {
-    }
-}
--- src/ProjectTemplate.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProjectTemplate.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
